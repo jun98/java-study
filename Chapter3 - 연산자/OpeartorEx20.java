a8722f54447730442439bb5721e55ee5e3a10a88
Jang wonjun <devwonjun@gmail.com>
2017-11-14 00:35:48 +0900

    산술연산자 #2

---
+++ Chapter3 - 연산자/OpeartorEx20.java
@@ -0,0 +1,8 @@
+class OpeartorEx20 {
+    public static void main(String args[]) {
+        System.out.println(-10%8);
+        System.out.println(10%-8);
+        System.out.println(10%8);
+        System.out.println(-10%-8);
+    }
+}
